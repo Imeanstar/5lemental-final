adb6b5d2d8546e5b7dd2dbc0f586f99e1c27bc51
h2s0 <h2s0_@naver.com>
2023-09-23 23:59:39 +0900

    🎨 MenuBox.jsx : 여백, 간격 조절, 이미지 깨짐 이슈 해결, 글씨 크기 수정

--- src/components/MenuBox.jsx
+++ src/components/MenuBox.jsx
@@ -13,24 +13,24 @@ function MenuBox({ item, name }) {
     <Link to="/recipedetail" onClick={() => keepMenuName(name)}>
       <li
         key={item.id}
-        className="px-[15px] pt-[11px] pb-[12px] mt-3 mb-3 -bg--fridge-bg-gray rounded-[10px] h-[94px] gap-[14px] flex justify-start"
+        className="px-[15px] py-3 -bg--fridge-bg-gray rounded-[10px] gap-[14px] flex justify-start"
       >
         <div className="figureWrapper">
           <figure className="w-[70px] h-[70px]">
             <img
               src={getPbImageURL(item, 'photo')}
-              alt=""
-              className="w-[70px] h-[70px] rounded-[10px]"
+              alt={item.name}
+              className="object-cover w-[70px] h-[70px] rounded-[10px]"
             />
           </figure>
         </div>
         <div className="descriptionWrapper flex flex-col pb-3">
           <div className="nameContainer">
-            <span className="inline-block -bg--fridge-white text-[12px] rounded-[15px] font-dohyeon px-[19px] pt-[4px] pb-[2px]">
+            <span className="inline-block -bg--fridge-white text-[15px] leading-6 rounded-[15px] font-dohyeon px-[19px]">
               {item.name}
             </span>
           </div>
-          <span className="font-nanum ml-1 text-[12px] mt-1">
+          <span className="font-nanum ml-2 mt-2 text-[13px]">
             {item.summary}
           </span>
         </div>

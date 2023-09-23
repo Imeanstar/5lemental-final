3ba58a017d9d88dc44b17a826eb42f95c72fc94e
h2s0 <h2s0_@naver.com>
2023-09-23 15:08:22 +0900

    🚑 콘텐츠가 하단 네비게이션에 가려지던 이슈 해결

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,12 +1,20 @@
 import NavBar from '@/components/navBar/NavBar';
 import Heading from '@/components/Heading';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function RootLayout() {
+  const { pathname } = useLocation();
+
+  let isHiddenPadding = null;
+
+  if ( pathname === '/' || pathname === '/signin' || pathname === 'signup') {
+    isHiddenPadding = null;
+  }
+
   return (
     <>
       <Heading />
-      <main>
+      <main className={!isHiddenPadding ? 'pb-20' : isHiddenPadding}>
         <Outlet />
       </main>
       <NavBar />

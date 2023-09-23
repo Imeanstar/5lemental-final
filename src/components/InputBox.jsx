5df7942e86e9cf803bf4728d369f90f480848105
dani-day <allkite5122@gmail.com>
2023-09-23 18:39:52 +0900

    ♻️ SignIn 페이지 useRef 사용하여 코드 리팩토링 완료

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,15 +1,21 @@
-function InputBox({ type = 'text', name = null, id = null, ...restProps }) {
+import { forwardRef } from 'react';
+
+function InputBox(
+  { type = 'text', name = null, id = null, ...restProps },
+  ref
+) {
   return (
-    <div>
+    <>
       <input
         className="border-[1px] rounded-[15px] py-[11px] px-[20px] min-w-full font-nanum text-[13px] -text--fridge-black placeholder:-text--fridge-input-gray placeholder:font-normal -border--fridge-gray  mb-1"
         type={type}
         name={name}
         id={id}
+        ref={ref}
         {...restProps}
       />
-    </div>
+    </>
   );
 }
 
-export default InputBox;
+export default forwardRef(InputBox);

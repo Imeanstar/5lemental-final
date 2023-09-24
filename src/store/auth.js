5af3c619597346911a19b4c362cf2948b833d0e2
Imeanstar <mike4404@naver.com>
2023-09-24 20:04:06 +0900

    ✨ auth.js 새로고침해도 로그아웃 안되도록 기능 추가

--- src/store/auth.js
+++ src/store/auth.js
@@ -13,9 +13,30 @@ const initialAuthState = {
 const authStore = (set) => ({
   ...initialAuthState,
 
+  initialize: () => {
+    // 화면 새로고침 시 로그인 상태 초기화
+    const token = localStorage.getItem('token');
+    if (token) {
+      pb.setToken(token); // Pocketbase에 토큰 설정
+      set({ isLoggedIn: true, token });
+    }
+  },
+
   /* Pb SDK를 사용한 회원가입 */
   signUp: async (newUser) => {
-    return await pb.collection(USER_COLLECTION).create(newUser);
+    const authData = await pb.collection(USER_COLLECTION).create(newUser);
+    const { token } = pb.authStore;
+    
+    // 토큰을 로컬 스토리지에 저장
+    localStorage.setItem('token', token);
+
+    set((state) => ({
+      ...state,
+      isLoggedIn: true,
+      token,
+    }));
+
+    return authData;
   },
 
   /* Pb SDK를 사용한 로그인 */
@@ -23,14 +44,15 @@ const authStore = (set) => ({
     const authData = await pb
       .collection(USER_COLLECTION)
       .authWithPassword(id, password);
+    const { token } = pb.authStore;
 
-    const { isValid, model, token } = pb.authStore;
+    // 토큰을 로컬 스토리지에 저장
+    localStorage.setItem('token', token);
 
     set((state) => ({
       ...state,
       id,
-      isValid,
-      user: model.id,
+      isLoggedIn: true,
       token,
     }));
 
@@ -40,6 +62,9 @@ const authStore = (set) => ({
   /* Pb SDK를 사용한 로그아웃 */
   signOut: async () => {
     const response = await pb.authStore.clear();
+
+    // 로컬 스토리지에서 토큰 제거
+    localStorage.removeItem('token');
 
     set((state) => ({
       ...state,
